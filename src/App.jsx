83d9cda51c1fb307add4392615d4eaf54d9280c4
Rizkydba <astrarizky@gmail.com>
2026-06-25 16:19:30 +0700

    feat: create application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-green-100">
-      <h1 className="text-5xl font-bold text-green-700">
-        Greenate 🌱
-      </h1>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/router/index.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+    ],
+  },
+]);
+
+export default router;
